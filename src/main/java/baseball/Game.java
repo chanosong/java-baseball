ece78f69b9261cd7379412774ca261f2afdb2e3f
chener <49267211+chanosong@users.noreply.github.com>
2022-11-08 23:25:17 +0900

    Squashed commit of the following:
    commit 8b512c351324972aefd06888ee5b9708174c4ea1
    Author: chener <49267211+chanosong@users.noreply.github.com>
    Date:   Tue Nov 8 23:24:52 2022 +0900
    
        Feat(Game) : initGame func
    
        + initGame(): initialize game function
        ~ askRestart(): Change checking method of user's decide
    
    commit 2fe1fb3cefd27e3361ba8a7daf46f5850d51c948
    Author: chener <49267211+chanosong@users.noreply.github.com>
    Date:   Tue Nov 8 23:11:41 2022 +0900
    
        Feat(Game): Drop useless newline
    
        - Drop useless newline
    
    commit 926366553a28c6e7ed43d6cd416554d14a95728a
    Author: chener <49267211+chanosong@users.noreply.github.com>
    Date:   Tue Nov 8 23:08:37 2022 +0900
    
        Feat(Game) : askRestart, convertString func
    
        + askRestart(): Ask player if he want restart game or end
        + convertString(): convert String to List<Integer>

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,13 +8,18 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Game {
-    // TODO: <Local Value> Game Class
     private List<Integer> answer;
     private BallCount ballcount;
 
+    // Initializer
     public Game() {
         ballcount = new BallCount();
+    }
+
+    // <Func> : Initialize new game
+    public void initGame() {
         setAnswer();
+        ballcount.initCount();
     }
 
     // <Func> : Set random answer using pickNumberInRange()
@@ -87,19 +92,51 @@ public class Game {
         return true;
     }
 
+    // <Func> : Convert String to List<Integer>
+    public List<Integer> convertString(String input) {
+        List<Integer> inputToListInt = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            inputToListInt.add(Character.getNumericValue(input.charAt(i)));
+        }
+
+        return inputToListInt;
+    }
+
+    public boolean askRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String decide;
+        while (true) {
+            decide = readLine();
+            if (decide.equals("1")) {
+                return true;
+            } else if (decide.equals("2")) {
+                return false;
+            } else {
+                System.out.println("1과 2중 하나만을 입력해주세요.");
+            }
+        }
+    }
+
     // <Func> : Run game
     public void run() {
+        boolean keepGoing = true;
 
-        while (isOver() == false) {
-            String inputNum = readLine();
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
-            checkInputFormat(inputNum);
+        while (keepGoing == true) {
+            initGame();
+            while (isOver() == false) {
+                System.out.print("숫자를 입력해주세요 : ");
+                String inputNum = readLine();
 
-            List<Integer> temp = new ArrayList<>();
-            for (int i = 0; i < 3; i++) {
-                temp.add(Character.getNumericValue(inputNum.charAt(i)));
+                checkInputFormat(inputNum);
+
+                List<Integer> inputNumList = convertString(inputNum);
+                checkAnswer(inputNumList);
             }
-            checkAnswer(temp);
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            keepGoing = askRestart();
         }
     }
 }
